3db8350887cbff1051445167826f81aaedc6949f
perezalexreal <perezalexreal@gmail.com>
2022-02-15 16:00:30 +0100

    Array Creato

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+$days = [
+    '12/02/2021' => [
+        [
+            'homeTeam' => 'Toronto',
+            'guestTeam' => 'NYC',
+            'pointsHomeTeam' => 14,
+            'pointsGuestTeam' => 32
+        ],
+        [
+            'homeTeam' => 'Bulls',
+            'guestTeam' => 'Lakers',
+            'pointsHomeTeam' => 24,
+            'pointsGuestTeam' => 24
+        ]
+    ],
+    '13/02/2021' => [
+        [
+            'homeTeam' => 'Warriors',
+            'guestTeam' => 'Boston',
+            'pointsHomeTeam' => 4,
+            'pointsGuestTeam' => 42
+        ],
+        [
+            'homeTeam' => 'Portland',
+            'guestTeam' => 'Miami',
+            'pointsHomeTeam' => 30,
+            'pointsGuestTeam' => 22
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<h1> Partite NBA della settimana</h1>
+
+<p>  </p>
+
+</body>
+
+</html>
